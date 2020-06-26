abe73f5d851b2582c3c215b4910f3d6da73da70f
Joona Yoon <joonas.yoon@gmail.com>
2020-06-27 08:50:24 +0900

    record stream

--- js/background.js
+++ js/background.js
@@ -1,20 +1,42 @@
 class translator {
     constructor(tab, stream) {
+    let self = this;
+  
         this.tab = tab;
         this.stream = stream;
         this.visualTimerId = this.visualize();
         
+    this.chunks = [];
+    this.mediaRecorder = new MediaRecorder(stream);
+    this.mediaRecorder.ondataavailable = function(e) {
+      console.log(self.chunks);
+      self.chunks.push(e.data);
+    }
+    
         var audioElement = document.createElement('audio');
         audioElement.setAttribute("preload", "auto");
         audioElement.autobuffer = true;
         audioElement.srcObject = this.stream;
         audioElement.load();
         audioElement.play();
+    this.audio = audioElement;
+
+    this.startRecord();
+    setTimeout(function(){
+      self.stopRecord();
+    }, 5000);
+    setTimeout(function(){
+      self.play();
+    }, 8000);
     }
 
     get active() {
         return this.stream && this.stream.active;
     }
+
+  get state() {
+    return this.active && this.mediaRecorder && this.mediaRecorder.state;
+  }
 
     uncapture() {
         clearInterval(this.visualTimerId);
@@ -32,7 +54,7 @@ class translator {
         var dataArray = new Float32Array(bufferLength);
         source.connect(analyser);
 
-        function test(self){
+    function requestDraw(self){
             analyser.getFloatTimeDomainData(dataArray);
             const sum = dataArray.reduce((a, b) => Math.abs(a) + Math.abs(b), 0);
             const avg = (sum / dataArray.length) || 0;
@@ -40,8 +62,43 @@ class translator {
             chrome.tabs.sendMessage(self.tab.id, {method: 'visualize', avgDecibel: percentage});
         }
         
-        return setInterval(test.bind(null, this), 200);
+    return setInterval(requestDraw.bind(null, this), 200);
     }
+
+  startRecord() {
+    console.log('state', this.state);
+    this.mediaRecorder.start();
+  }
+
+  stopRecord() {
+    console.log('state', this.state);
+    this.mediaRecorder.stop();
+    console.log(this.audio);
+    this.audio.pause();
+  }
+
+  play() {
+    let audio = document.createElement('audio');
+    audio.controls = true;
+    let blob = new Blob(this.chunks, { 'type' : 'audio/ogg; codecs=opus' });
+    this.chunks = [];
+    let audioURL = window.URL.createObjectURL(blob);
+    console.log('chunk', this.chunks);
+    console.log('blob', blob);
+    console.log('audioURL', audioURL);
+    audio.src = audioURL;
+    console.log(audio);
+    audio.play();
+
+    this.send(blob);
+  }
+  
+  send(audio) {
+    var fileData = new FormData();
+    fileData.append('audio', audio);
+    fileData.append('filename', 'random_text');
+    console.log(fileData);
+  }
 };
 
 let clients = {};
