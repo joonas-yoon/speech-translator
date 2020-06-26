c6772c7bdeab176e4e337cfe82f902c6d9e30c64
Joona Yoon <joonas.yoon@gmail.com>
2020-06-27 08:51:07 +0900

    convert indentation using 4 spaces

--- js/background.js
+++ js/background.js
@@ -1,11 +1,11 @@
 class translator {
-    constructor(tab, stream) {
+  constructor(tab, stream) {
     let self = this;
   
-        this.tab = tab;
-        this.stream = stream;
-        this.visualTimerId = this.visualize();
-        
+    this.tab = tab;
+    this.stream = stream;
+    this.visualTimerId = this.visualize();
+    
     this.chunks = [];
     this.mediaRecorder = new MediaRecorder(stream);
     this.mediaRecorder.ondataavailable = function(e) {
@@ -13,12 +13,12 @@ class translator {
       self.chunks.push(e.data);
     }
     
-        var audioElement = document.createElement('audio');
-        audioElement.setAttribute("preload", "auto");
-        audioElement.autobuffer = true;
-        audioElement.srcObject = this.stream;
-        audioElement.load();
-        audioElement.play();
+    var audioElement = document.createElement('audio');
+    audioElement.setAttribute("preload", "auto");
+    audioElement.autobuffer = true;
+    audioElement.srcObject = this.stream;
+    audioElement.load();
+    audioElement.play();
     this.audio = audioElement;
 
     this.startRecord();
@@ -28,42 +28,42 @@ class translator {
     setTimeout(function(){
       self.play();
     }, 8000);
-    }
+  }
 
-    get active() {
-        return this.stream && this.stream.active;
-    }
+  get active() {
+    return this.stream && this.stream.active;
+  }
 
   get state() {
     return this.active && this.mediaRecorder && this.mediaRecorder.state;
   }
 
-    uncapture() {
-        clearInterval(this.visualTimerId);
-        this.stream.getTracks().forEach(function(track){
-            track.stop();
-        });
-    }
+  uncapture() {
+    clearInterval(this.visualTimerId);
+    this.stream.getTracks().forEach(function(track){
+      track.stop();
+    });
+  }
 
-    visualize() {
-        var audioCtx = new (window.AudioContext || webkitAudioContext)();
-        var source = audioCtx.createMediaStreamSource(this.stream);
-        var analyser = audioCtx.createAnalyser();
-        analyser.fftSize = 2048;
-        var bufferLength = analyser.frequencyBinCount;
-        var dataArray = new Float32Array(bufferLength);
-        source.connect(analyser);
+  visualize() {
+    var audioCtx = new (window.AudioContext || webkitAudioContext)();
+    var source = audioCtx.createMediaStreamSource(this.stream);
+    var analyser = audioCtx.createAnalyser();
+    analyser.fftSize = 2048;
+    var bufferLength = analyser.frequencyBinCount;
+    var dataArray = new Float32Array(bufferLength);
+    source.connect(analyser);
 
     function requestDraw(self){
-            analyser.getFloatTimeDomainData(dataArray);
-            const sum = dataArray.reduce((a, b) => Math.abs(a) + Math.abs(b), 0);
-            const avg = (sum / dataArray.length) || 0;
-            const percentage = Math.max(0, Math.min(1.0, Math.abs(avg * 10)));
-            chrome.tabs.sendMessage(self.tab.id, {method: 'visualize', avgDecibel: percentage});
-        }
-        
-    return setInterval(requestDraw.bind(null, this), 200);
+      analyser.getFloatTimeDomainData(dataArray);
+      const sum = dataArray.reduce((a, b) => Math.abs(a) + Math.abs(b), 0);
+      const avg = (sum / dataArray.length) || 0;
+      const percentage = Math.max(0, Math.min(1.0, Math.abs(avg * 10)));
+      chrome.tabs.sendMessage(self.tab.id, {method: 'visualize', avgDecibel: percentage});
     }
+    
+    return setInterval(requestDraw.bind(null, this), 200);
+  }
 
   startRecord() {
     console.log('state', this.state);
@@ -105,27 +105,27 @@ let clients = {};
 
 // Respond to the click on extension Icon
 chrome.browserAction.onClicked.addListener(function (tab) {
-    console.log(tab, clients);
+  console.log(tab, clients);
 
-    // uncapture if already captured
-    if (clients[tab.id] && clients[tab.id].active) {
-        clients[tab.id].uncapture();
-        delete clients[tab.id];
-        chrome.tabs.sendMessage(tab.id, {method: 'hideViewer'});
-    } else {
-        // capture a stream from current tab
-        chrome.tabCapture.capture(
-            {video: false, audio: true},
-            function (stream) {
-                if (!stream) {
-                    console.error('Error starting tab capture: ' +
-                                (chrome.runtime.lastError.message || 'UNKNOWN'));
-                    return;
-                }
+  // uncapture if already captured
+  if (clients[tab.id] && clients[tab.id].active) {
+    clients[tab.id].uncapture();
+    delete clients[tab.id];
+    chrome.tabs.sendMessage(tab.id, {method: 'hideViewer'});
+  } else {
+    // capture a stream from current tab
+    chrome.tabCapture.capture(
+      {video: false, audio: true},
+      function (stream) {
+        if (!stream) {
+          console.error('Error starting tab capture: ' +
+                (chrome.runtime.lastError.message || 'UNKNOWN'));
+          return;
+        }
 
-                clients[tab.id] = new translator(tab, stream);
-                chrome.tabs.sendMessage(tab.id, {method: 'showViewer'});
-            }
-        );
-    }
+        clients[tab.id] = new translator(tab, stream);
+        chrome.tabs.sendMessage(tab.id, {method: 'showViewer'});
+      }
+    );
+  }
 });
