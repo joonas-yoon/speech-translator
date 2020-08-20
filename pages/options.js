a9bb6a81c55c6034ebb84156303562441daa4dd9
Joona Yoon <joonas.yoon@gmail.com>
2020-08-20 23:49:13 +0900

    Get list of supported languages from server

--- pages/options.js
+++ pages/options.js
@@ -1,3 +1,5 @@
+const API_URL = 'http://localhost/api';
+
 let srcLang = document.getElementById('src-lang');
 let dstLang = document.getElementById('dst-lang');
 let srcLangList = srcLang.querySelector('.select-langs');
@@ -5,13 +7,15 @@ let dstLangList = dstLang.querySelector('.select-langs');
 let srcLangInput = srcLang.querySelector('input');
 let dstLangInput = dstLang.querySelector('input');
 
-function createLangaugeItem(name) {
+function createLangaugeItem(code, name) {
     let lang = document.createElement('div');
     lang.className = 'lang';
     lang.innerText = name;
+    lang.data_code = code;
     lang.addEventListener('click', (evt) => {
         let input = evt.target.parentElement.parentElement.querySelector('input[type=text]');
         input.value = lang.innerText;
+        input.value_code = lang.data_code;
         input.dispatchEvent(new Event('keyup'));
     });
     return lang;
@@ -19,6 +23,7 @@ function createLangaugeItem(name) {
 
 function onInputChanged(evt) {
     let e = evt.target;
+    console.log(e);
     let langs = e.parentElement.querySelectorAll('.select-langs > .lang');
     let str = e.value.toLowerCase();
     langs.forEach(function (val, idx) {
@@ -42,34 +47,51 @@ function hideLangList(evt) {
     setTimeout(function(){ langList.style.display = 'none'; }, 100);
 }
 
-function save() {
-    console.log('save!');
-    addAlertMessage('All saved successfully!')
+function load() {
+    $.ajax({
+        url: API_URL + '/app/translate/supports',
+        data: {},
+        method: 'GET',
+        dataType: 'json',
+    })
+    .done(function(langs){
+        for (var i = 0; i < langs.length; i++) {
+            srcLangList.appendChild(createLangaugeItem(langs[i].code, langs[i].name));
+            dstLangList.appendChild(createLangaugeItem(langs[i].code, langs[i].name));
+        }
+        srcLangInput.addEventListener('keyup', onInputChanged);
+        dstLangInput.addEventListener('keyup', onInputChanged);
+        srcLangInput.addEventListener('focus', showLangList);
+        dstLangInput.addEventListener('focus', showLangList);
+        srcLangInput.addEventListener('focusout', hideLangList);
+        dstLangInput.addEventListener('focusout', hideLangList);
+        document.getElementById('btn-save').removeAttribute('disabled');
+    })
+    .fail(function(xhr, status, errorThrown){
+        console.error(xhr, status, errorThrown);
+        addAlertMessage('Server Error', 'fail', true);
+    });
 }
 
-function addAlertMessage(msg, cls) {
+function save() {
+    addAlertMessage('All saved successfully!');
+}
+
+function addAlertMessage(msg, cls, keep) {
     let alerts = document.getElementById('alerts');
     let box = document.createElement('div');
     box.className = 'alert ' + (cls || '');
     box.innerHTML = msg;
     alerts.appendChild(box);
-    setTimeout(function(){
-        box.remove();
-    }, 3000);
+    if (!keep) {
+        setTimeout(function(){
+            box.remove();
+        }, 3000);
+    }
 }
 
 document.addEventListener('DOMContentLoaded', function(){
-    let langs = ['Arabic', 'Chinese', 'Danish', 'Dutch', 'English', 'French', 'German', 'Greek', 'Hungarian', 'Italian', 'Japanese', 'Korean', 'Lithuanian', 'Persian', 'Polish', 'Portuguese', 'Russian', 'Spanish', 'Swedish', 'Turkish', 'Vietnamese'];
-    for (var i = 0; i < langs.length; i++) {
-        srcLangList.appendChild(createLangaugeItem(langs[i]));
-        dstLangList.appendChild(createLangaugeItem(langs[i]));
-    }
-    srcLangInput.addEventListener('keyup', onInputChanged);
-    dstLangInput.addEventListener('keyup', onInputChanged);
-    srcLangInput.addEventListener('focus', showLangList);
-    dstLangInput.addEventListener('focus', showLangList);
-    srcLangInput.addEventListener('focusout', hideLangList);
-    dstLangInput.addEventListener('focusout', hideLangList);
+    load();
 
     document.getElementById('btn-save').addEventListener('click', function(evt){
         evt.preventDefault;
